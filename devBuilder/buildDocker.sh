904549d2ff5c81542e0b4e4091b8c7bf7a917b71
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-03-29 11:25:38 +0100

    Allow building test image from several fragments

--- devBuilder/buildDocker.sh
+++ devBuilder/buildDocker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# Build a docker image from a file
+# Build a docker image from a file or several fragments
 
-mkdir dockertest
-cp $1 dockertest/Dockerfile
+mkdir -p dockertest
+# Make an empty file
+echo "" > dockertest/Dockerfile
+
+for var in "$@"
+do
+	echo "Adding $var to recipe"
+	cat "$var" >> dockertest/Dockerfile
+done
 
 docker build dockertest -t research-stacks/dockertest
 
