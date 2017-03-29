3d5fd3233ecaf2bae5fbd5a1ae349c55d2f4cdc7
Andrew Walker <a.walker@leeds.ac.uk>
2017-03-29 11:40:17 +0100

    Return pre-built dockerfile to user

--- scistack/scistack.py
+++ scistack/scistack.py
@@ -7,12 +7,23 @@ SciStack: the web app to build docker containers for reproducable science
 """
 import os
 import flask
+import inspect
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
+
+# Home of any pre-build docker files
+docker_file_path = os.path.join(os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe()))), "..", "dockerfiles")
 
 @app.route("/")
 def hello():
     return "Choose a domain!"
+
+@app.route("/dfview/<path:fname>")
+def send_dockerfile(fname):
+    with open(os.path.join(docker_file_path, fname)) as dfile:
+        return_value = dfile.read()
+    return return_value
 
 if __name__ == "__main__":
     app.run()
